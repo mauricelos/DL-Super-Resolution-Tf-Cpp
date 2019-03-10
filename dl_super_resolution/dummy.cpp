35700a9c81edd8fdf438c958a8fa8f6d0474fa7c
Maurice Loskyll <maurice@loskyll.de>
2019-03-11 00:39:26 +0100

    Add tensorflow as external dependency and add dummy.cpp for testing dependencies

---
+++ dl_super_resolution/dummy.cpp
@@ -0,0 +1,22 @@
+#include "tensorflow/cc/client/client_session.h"
+#include "tensorflow/cc/ops/standard_ops.h"
+#include "tensorflow/core/framework/tensor.h"
+
+int main()
+{
+    using namespace tensorflow;
+    using namespace tensorflow::ops;
+    Scope root = Scope::NewRootScope();
+    auto a = Placeholder(root, DT_INT32);
+    auto c = Add(root, a, {41});
+
+    ClientSession session(root, "test");
+    std::vector<Tensor> outputs;
+
+    Status s = session.Run({{a, {1}}}, {c}, &outputs);
+    if (!s.ok())
+    {
+        LOG(INFO) << "Shit";
+    }
+    return 0;
+}
