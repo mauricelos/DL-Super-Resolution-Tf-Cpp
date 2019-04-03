dc22dee784ccb8305bda1816489104276afa56ee
Maurice Loskyll <maurice@loskyll.de>
2019-04-03 21:41:45 +0200

    Add unit tests for constructor and EndsWith function

--- dl_super_resolution/test/dl_model_helper_unit_tests.cpp
+++ dl_super_resolution/test/dl_model_helper_unit_tests.cpp
@@ -1,8 +1,59 @@
 #include "gtest/gtest.h"
 
+#define private public
 #include "dl_super_resolution/include/dl_model_helper.h"
 
-TEST(DlModelHelperTest, DummyTest)
+class DlModelHelperTest : public testing::Test
 {
-    EXPECT_TRUE(true);
+  public:
+    DlModelHelper unit_{};
+};
+
+TEST_F(DlModelHelperTest, DlModelHelper_DefaultConstructorTest)
+{
+    const std::uint32_t expected_input_height{1080U};
+    const std::uint32_t expected_input_width{1920U};
+    const std::uint32_t expected_num_required_image_channels{3U};
+
+    EXPECT_EQ(unit_.input_height_, expected_input_height);
+    EXPECT_EQ(unit_.input_width_, expected_input_width);
+    EXPECT_EQ(unit_.num_required_image_channels_, expected_num_required_image_channels);
+}
+
+TEST_F(DlModelHelperTest, DlModelHelper_CustomConstructorTest)
+{
+    const std::uint32_t expected_input_height{900U};
+    const std::uint32_t expected_input_width{1600U};
+    const std::uint32_t expected_num_required_image_channels{2U};
+    DlModelHelper temp_unit{expected_input_height, expected_input_width, expected_num_required_image_channels};
+
+    EXPECT_EQ(temp_unit.input_height_, expected_input_height);
+    EXPECT_EQ(temp_unit.input_width_, expected_input_width);
+    EXPECT_EQ(temp_unit.num_required_image_channels_, expected_num_required_image_channels);
+}
+
+TEST_F(DlModelHelperTest, EndsWith_FilenameEndsWithPNGSuffix)
+{
+    const std::string file_name{"image_test.png"};
+    const std::string file_suffix{".png"};
+
+    EXPECT_TRUE(unit_.EndsWith(file_name, file_suffix));
+}
+
+TEST_F(DlModelHelperTest, EndsWith_FilenameEndsWithJPEGSuffix)
+{
+    const std::string file_name{"image_test.jpeg"};
+    const std::string file_suffix{".jpeg"};
+
+    EXPECT_TRUE(unit_.EndsWith(file_name, file_suffix));
+}
+
+TEST_F(DlModelHelperTest, EndsWith_FilenameEndsWithDifferentSuffix)
+{
+    const std::string file_name{"image_test.bmp"};
+    const std::string file_suffix{".jpeg"};
+    const std::string alternative_file_suffix{".png"};
+
+    EXPECT_FALSE(unit_.EndsWith(file_name, file_suffix));
+    EXPECT_FALSE(unit_.EndsWith(file_name, alternative_file_suffix));
 }
